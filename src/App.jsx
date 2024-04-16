84750192bb495d521bbb2d60fc895eda8f9b594e
Sandeesh <sandeeshc@techprovint.com>
2024-04-16 09:30:18 +0530

    Add player change with symbols for the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import Player from "./components/Player"
+import GameBoard from './components/GameBoard'
+import { useState } from 'react';
 
-function App() {
-  
-
+function App()
+{
+    const [activePlayer, setActivePlayer] = useState('X');
+    function handleSelectSquare()
+    {
+        setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' )
+    }
   return (
-    <main>React Tic-Tac-Toe
-      <div id='game-container'></div>
+    <main>
+      <div id='game-container'>
+        <ol id="players" className='highlight-player'>
+            <Player initialName="Player 1" isActive={ activePlayer === 'X' } symbol="X"/>
+            <Player initialName="Player 2" isActive={ activePlayer === 'O' } symbol="O"/>
+        </ol>
+              <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={ activePlayer} />
+      </div>
     </main>
   )
 }
